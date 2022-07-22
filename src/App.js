ee3d67a152a500a94b2807f52ae5aab784340d20
Daniel <dychen2021@gmail.com>
2022-07-21 23:47:45 -0700

    finished mvp part 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, {useState,useEffect} from 'react';
 import axios from 'axios';
 import NasaPhoto from './Components/NasaPhoto';
+import styled from 'styled-components';
 import "./App.css";
 
-const dummyData = {
-  date: 'x',
-  explanation: 'y',
-  hdurl: 'z',
-  title: 'k'
-}
+// const dummyData = {
+//   date: 'x',
+//   explanation: 'y',
+//   hdurl: 'z',
+//   title: 'k'
+// }
+
+const NasaSection = styled.div`
+  background-color: magenta;
+  width: 60%;
+  height: 70%;
+`;
+
 
 function App() {
   const [data, setData] = useState();
@@ -20,9 +28,9 @@ function App() {
     .catch(err => console.log(err))
   })
   return (
-    <div className="App">
+    <NasaSection>
       <NasaPhoto photo={data} />
-    </div>
+    </NasaSection>
   );
 }
 
--- src/Components/NasaPhoto.js
+++ src/Components/NasaPhoto.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const HeadThreeTitle = styled.h3`
+  font-style: bold;
+  color: purple;
+  font-size: 2rem;
+  margin-bottom: 2%;
+`;
 
 const NasaPhoto = (props) =>{
     return (
     <div className="nasa-photo-wrapper">
-        <h3>{props.photo.title}</h3>
+        <HeadThreeTitle>{props.photo.title}</HeadThreeTitle>
         <p>{props.photo.date}</p>
         <img src={props.photo.hdurl} alt="nasa img"/>
         <p className='explanation'>{props.photo.explanation}</p>
